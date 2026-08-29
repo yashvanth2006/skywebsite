983358b85fc9ae5cbb9780a8f3343ccfbe57feb6
yashvanth2006 <yashvanth2006k@gmail.com>
2026-08-29 16:01:45 +0530

    remastered

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  phone: { type: String, required: true },
+  password: { type: String, required: true, minlength: 6, select: false },
+  isAdmin: { type: Boolean, default: false },
+  address: {
+    street: String,
+    city: { type: String, default: 'Coimbatore' },
+    pincode: String
+  }
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
